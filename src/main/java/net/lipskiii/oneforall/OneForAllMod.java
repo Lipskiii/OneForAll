74445e0a01f863c1f40ac24ebbf7ce3e2b5741e5
Lipskiii <max.lipski@hotmail.com>
2022-04-07 17:14:29 +0200

    added first custom item

--- src/main/java/net/lipskiii/oneforall/OneForAllMod.java
+++ src/main/java/net/lipskiii/oneforall/OneForAllMod.java
@@ -1,10 +1,12 @@
 package net.lipskiii.oneforall;
 
 import com.mojang.logging.LogUtils;
+import net.lipskiii.oneforall.item.ItemInit;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.InterModComms;
 import net.minecraftforge.fml.common.Mod;
@@ -28,10 +30,12 @@ public class OneForAllMod
 
     public OneForAllMod()
     {
-        // Register the setup method for modloading
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+        IEventBus eventBus = FMLJavaModLoadingContext.get().getModEventBus();
 
-        // Register ourselves for server and other game events we are interested in
+        ItemInit.register(eventBus);
+
+        eventBus.addListener(this::setup);
+
         MinecraftForge.EVENT_BUS.register(this);
     }
 
